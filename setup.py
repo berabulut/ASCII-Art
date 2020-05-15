3254ffdc0214b65ed6225c364d0d736375db6ae1
berabulut <berabulut@gmail.com>
2020-05-15 18:06:21 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Ascii-Art',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='betab',
+    author_email='',
+    description=''
+)
